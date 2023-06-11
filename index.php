ad6fa3c93efc0032afd12b5f3a8a0aaa9a7a27e1
Aryayan Roy <aryayanroy0@gmail.com>
2023-06-11 11:11:31 +0530

    login page backend

--- index.php
+++ index.php
@@ -1,10 +1,35 @@
+<?php
+    session_start();
+
+    if($_SERVER["REQUEST_METHOD"]=="POST" && isset($_POST["login"], $_POST["username"], $_POST["password"])){
+        require "config.php";
+
+        $username = str_replace(" ", "", trim($_POST["username"]));
+        $password = trim($_POST["password"]);
+
+        $sql = $conn->prepare("SELECT * FROM admins WHERE username = ?");
+        $sql->bindParam(1, $username, PDO::PARAM_STR);
+        $sql->execute();
+        $admin = $sql->fetch(PDO::FETCH_ASSOC);
+
+        if($admin && password_verify($password, $admin["password"])){
+            $_SESSION["admin"] = $admin["id"];
+            $feedback = true;
+        }else{
+            session_destroy();
+            $feedback = false;
+        }
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php if(isset($feedback) && $feedback==true){echo "<meta http-equiv='refresh' content='2;url=dashboard'>";} ?>
     <title>Library Management</title>
+    <link rel="shortcut icon" href="assets/public/images/favicon.ico" type="image/x-icon">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -15,7 +40,7 @@
 <body>
     <div class="container-xxl">
         <div class="row">
-            <aside class="col-2 min-vh-100 border-end">
+            <aside class="d-none d-md-block col-3 col-xl-2 min-vh-100 border-end">
                 <div class="py-3 d-flex flex-column sticky-top h-100">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 55 64" width=50 class="mx-auto"><path d="M55 26.5v23.8c0 1.2-.4 2.2-1.3 3.2-.9.9-1.9 1.5-3.2 1.6-3.5.4-6.8 1.3-10.1 2.6S34 60.8 31 62.9a6.06 6.06 0 0 1-3.5 1.1 6.06 6.06 0 0 1-3.5-1.1c-3-2.1-6.1-3.9-9.4-5.2s-6.7-2.2-10.1-2.6c-1.3-.2-2.3-.7-3.2-1.6-.9-1-1.3-2-1.3-3.2V26.5c0-1.3.5-2.4 1.4-3.2s2-1.2 3.1-1c4 .6 8 2 11.9 4 3.9 2.1 7.6 4.8 11.1 8.1 3.5-3.3 7.2-6 11.1-8.1s7.9-3.4 11.9-4c1.2-.2 2.2.1 3.1 1 .9.8 1.4 1.9 1.4 3.2z" fill="#004d40"/><path d="M39.5 11.8c0 3.3-1.1 6.1-3.4 8.4s-5.1 3.4-8.4 3.4-6.1-1.1-8.4-3.4-3.4-5.1-3.4-8.4 1.1-6.1 3.4-8.4S24.4 0 27.7 0s6.1 1.1 8.4 3.4 3.4 5.1 3.4 8.4z" fill="#e65100"/></svg>
                     <hr>
@@ -30,29 +55,52 @@
                     </nav>
                 </div>
             </aside>
-            <main class="col-10 px-0">
+            <main class="col-md-9 col-xl-10 px-0">
                 <header class="navbar bg-white border-bottom sticky-top">
                     <div class="container-fluid">
-                        <a href="/" class="navbar-brand"><h3 class="mb-0"><i class="fa-solid fa-landmark me-3"></i><span>Library Management</span></h3></a>
+                        <a href="/" class="navbar-brand"><h3 class="mb-0"><i class="fa-solid fa-landmark me-3"></i><span class="d-none d-md-inline">Library Management</span></h3></a>
+                        <button type="button" class="btn d-md-none" data-bs-toggle="offcanvas" data-bs-target="#aside-right"><i class="fa-solid fa-bars fa-xl"></i></button>
                     </div>
                 </header>
                 <article class="container-fluid py-3">
-                    <form action="#" method="post" class="mx-auto my-5" style="width: 300px">
+                    <form action=<?php echo $_SERVER["PHP_SELF"]; ?> method="post" class="mx-auto mt-5" style="max-width: 350px">
                         <h3>Login</h3>
                         <div class="form-floating">
-                            <input type="text" class="form-control" placeholder="Login ID">
-                            <label for="#">Login ID</label>
+                            <input type="text" name="username" class="form-control" placeholder="Login ID">
+                            <label for="#">Username</label>
                         </div>
                         <div class="form-floating my-3">
-                            <input type="password" class="form-control" placeholder="Password">
+                            <input type="password" name="password" class="form-control" placeholder="Password">
                             <label for="#">Password</label>
                         </div>
-                        <div><button type="submit" class="btn btn-primary w-100">Login</button></div>
+                        <div><button type="submit" name="login" class="btn btn-primary w-100">Login</button></div>
+                        <?php if(isset($feedback)){echo "<div class='alert ".($feedback?"alert-success":"alert-danger")." mt-3'>".($feedback?"<i class='fa-regular fa-circle-check me-2'></i><span>Sucessfully logged in</span>":"<i class='fa-regular fa-circle-xmark me-2'></i><span>Invalid credentials</span>")."</div>";}?>
                     </form>
                 </article>
             </main>
         </div>
     </div>
+    <!-- Off canvas -->
+    <aside id="aside-right" class="offcanvas offcanvas-end">
+        <div class="offcanvas-header">
+            <h5 class="offcanvas-title text-truncate">Hello, Sabrina</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="offcanvas"></button>
+        </div>
+        <div class="offcanvas-body">
+            <nav class="nav nav-pills flex-column flex-grow-1">
+                <a href="dashboard" class="nav-link link-dark"><i class="fa-solid fa-gauge me-2"></i><span>Dashboard</span></a>
+                <a href="borrows" class="nav-link link-dark"><i class="fa-solid fa-list-check me-2"></i><span>Borrows</span></a>
+                <a href="books" class="nav-link link-dark"><i class="fa-solid fa-book me-2"></i><span>Books</span></a>
+                <a href="members" class="nav-link link-dark"><i class="fa-solid fa-users me-2"></i><span>Members</span></a>
+                <a href="shelves" class="nav-link link-dark"><i class="fa-solid fa-layer-group me-2"></i><span>Shelves</span></a>
+                <a href="transactions" class="nav-link link-dark"><i class="fa-solid fa-receipt me-2"></i><span>Transactions</span></a>
+                <a href="settings" class="nav-link link-dark"><i class="fa-solid fa-cog me-2"></i><span>Settings</span></a>
+            </nav>
+        </div>
+        <div class="p-3 border-top">
+            <a href="#" class="btn btn-light w-100">Logout</a>
+        </div>
+    </aside>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 </html>
